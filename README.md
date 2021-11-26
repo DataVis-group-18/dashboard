96c5b8b65409ea9f30b11f220f248f204035ef8d
Terts Diepraam <terts.diepraam@gmail.com>
2021-11-26 12:34:57 +0100

    start experimenting with shodan data

--- README.md
+++ README.md
@@ -1,41 +1,69 @@
 # Dashboard
 
-## How to use:
+## How to use
+> Note: The data is not committed on the `main` branch. It should be placed in the `public/data` folder.
+
 First install dependencies with:
-```
+```console
 npm install
 ```
 
 Then run the live reloading development server with:
-```
+```console
 npm run start
 ```
 
 To build a static website:
-```
+```console
 npm run build
 ```
 
-Deploying the site to GitHub Pages:
-```
+Deploying the site to GitHub Pages (includes uploading the data):
+```console
 ./deploy.sh
 ```
+
+This last command with build the site to `dist` and commit it to the `gh-pages` branch, which is hosted by GitHub Pages.
+
+## Tech Stack
+The following technologies/tools are used in this project:
+- [D3](https://d3js.org/) (for visualization)
+- [TypeScript](https://www.typescriptlang.org/) (for type checking)
+- [Vite](https://vitejs.dev/) (for dependency building)
+- [GitHub Pages](https://docs.github.com/en/pages/getting-started-with-github-pages/about-github-pages) (for hosting)
 
 ## Folder structure
 ```
 dashboard
-├── README.md         // this document
-├── deploy.sh         // script to deploy to GitHub pages
+├── README.md         // This document
+├── deploy.sh         // Script to deploy to GitHub pages
 │
 ├── package.json      // NPM package configuration
 ├── tsconfig.json     // TypeScript configuration
 ├── vite.config.js    // Vite configuration
 │
-├── public            // everything in public will be accessible on the page
+├── public            // Everything in public will be accessible on the page
+│  └── data           // Data should be put here
 │
 ├── index.html        // HTML entrypoint
 ├── src               // JS & CSS files
-│  ├── main.ts
+│  ├── main.ts        // TypeScript entrypoint
 │  └── style.css
 └── dist              // The output of `npm run build` will be put here
 ```
+
+## Notes on using Vite
+Vite is a bundling tool for JavaScript and TypeScript projects. It analyses all files that are used throughout the project and bundles them during the `npm run build` command. Therefore, Vite needs complete control over what is and what is not included into the final HTML.
+
+In many tutorials, you will see that `D3` is included using a `<script>` in the HTML, but we can let Vite take care of that by installing via NPM and just referencing it in the code. This leads (hopefully) to better load times of the website, but also to cleaner code.
+
+The same goes for CSS, we can import multiple CSS files in `main.ts`, which all get transformed into a single CSS files by Vite:
+```ts
+import './style.css';
+```
+This means that you don't have to manually add `<link>` tags into the HTML.
+
+
+
+
+
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './style.css';
+import "./style.css";
 import * as d3 from "d3";
 
 const svg = d3.select("svg");
@@ -6,15 +6,63 @@ const margin = 200;
 const width = parseInt(svg.attr("width")) - margin;
 const height = parseInt(svg.attr("height")) - margin;
 
-const x = d3.scaleBand().range([0, width]).padding(0.4);
+const x = d3.scaleLinear().range([0, width]);
 const y = d3.scaleLinear().range([height, 0]);
 
 const g = svg.append("g").attr("transform", "translate(100, 50)");
 
-d3.csv("test.csv").then((data) => {
-  x.domain(data.map((d) => d.year!));
+interface Row {
+  ip_str: string;
+  isp: string;
+  org: string;
+  os: string;
+  port: number;
+  timestamp: Date;
+  location: {
+    city: string;
+    region_code: string;
+    area_code: string | null;
+    longitude: number;
+    latitude: number;
+    postal_code: string | null;
+    country_code: string;
+    country_name: string;
+  };
+  vulns: { [key: string]: CVE } | null;
+}
 
-  y.domain([0, d3.max(data, (d) => d.value)] as number[]);
+interface CVE {
+  verified: boolean;
+  cvss: string | number;
+  summary: string;
+  references: string[];
+}
+
+const long = (d: Row): number => d.location.longitude;
+const lat = (d: Row): number => d.location.latitude;
+const longlat = (d: Row): [number, number] => [long(d), lat(d)];
+
+d3.json<Row[]>("data/clean_shodan_delft.json").then((data) => {
+  if (data === undefined) {
+    return;
+  }
+
+  let counts = new Map<number, number>();
+  for (const row of data) {
+    if (!row.vulns) {
+      continue;
+    }
+    let vulns = Object.keys(row.vulns).length;
+    let count = counts.get(vulns) || 0;
+    counts.set(vulns, count + 1);
+  }
+
+  const min = Math.min(...counts.values());
+  const max = Math.max(...counts.values());
+  x.domain([Math.min(...counts.keys()), Math.max(...counts.keys())]);
+  y.domain([Math.min(...counts.values()), Math.max(...counts.values())]);
+
+  console.log(counts);
 
   g.append("g")
     .attr("transform", "translate(0," + height + ")")
@@ -37,28 +85,22 @@ d3.csv("test.csv").then((data) => {
     .text("y label");
 
   g.selectAll(".bar")
-    .data(data)
+    .data(counts.entries())
     .enter()
     .append("rect")
-    .on("mouseover", function() {
-      d3.select(this)
-        .transition()
-        .delay(0.5)
-        .style("fill", "orange");
+    .on("mouseover", function () {
+      d3.select(this).transition().delay(0.5).style("fill", "orange");
     })
-    .on("mouseout", function() {
-      d3.select(this)
-        .transition()
-        .delay(0.5)
-        .style("fill", "black");
+    .on("mouseout", function () {
+      d3.select(this).transition().delay(0.5).style("fill", "black");
     })
     .attr("class", "bar")
-    .attr("x", (d) => x(d.year!)!)
-    .attr("y", (d) => y(parseInt(d.value!)))
-    .attr("width", x.bandwidth())
+    .attr("x", (v) => x(v[0]))
+    .attr("y", (v) => y(v[1]))
+    .attr("width", width / x.range()[1])
     .transition()
     .ease(d3.easeLinear)
     .duration(400)
     .delay((d, i) => i * 50)
-    .attr("height", (d) => height - y(parseInt(d.value!)));
+    .attr("height", (v) => height - y(v[1]));
 });
